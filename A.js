e7a17b5a2a14fa2d00d271d738eae7167d4999bb
Spacenaut47 <sohammondal81@gmail.com>
2022-04-02 11:34:05 +0530

    Adding files

---
+++ A.js
@@ -0,0 +1,41 @@
+/*----------------- DECLARATIONS -----------------*/
+
+let first = 10;let second = 4;
+let cards = [first,second]
+let sum = cards[0]+cards[1]; 
+let blackjack = false;let alive = true;
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardEl = document.querySelector("#cards-el")
+
+/*----------------- Rendering the cards -----------------*/
+
+function renderGame(){
+    if(sum <= 20){
+        message = "Do you want to draw a new card?";
+    }else if(sum === 21){
+        blackjack = true;
+        message = "You've got blackjack";
+    }else {
+        alive = false;
+        message = "Youre out of the game";
+    }
+    sumEl.textContent = "Sum: "+sum;
+    cardEl.textContent = "Cards: ";
+    for(let i = 0; i < cards.length;i++){
+        cardEl.textContent += cards[i] + " ";
+    }
+    messageEl.textContent = message;
+}
+
+function start(){
+    renderGame()
+}
+
+function newCard(){
+    let newcard =+ 7
+    sum += newcard
+    cards.push(newcard)
+    renderGame()
+}
